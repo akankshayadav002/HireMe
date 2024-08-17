1ed7614c4ea7dc53a5b80f303c6881542175304e
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2024-08-17 19:58:19 +0530

    vercel server details

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                         !user ? (
                             <div className='flex items-center gap-2'>
                                 <Link to="/login"><Button variant="outline">Login</Button></Link>
-                                <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#FF7F50]">Signup</Button></Link>
+                                <Link to="/signup"><Button className="bg-[#FF7F50] hover:bg-[#40E0D0]">Signup</Button></Link>
                             </div>
                         ) : (
                             <Popover>
